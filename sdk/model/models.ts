eb2d8fec1a85b098f5ad105a0c2f053ce37bdadd
viktor-sakhno-saritasa <viktor-sakhno-saritasa@users.noreply.github.com>
2023-07-25 11:22:12 +0000

    Update SDK

---
+++ sdk/model/models.ts
@@ -0,0 +1,6 @@
+export * from './api-response.dto';
+export * from './category.dto';
+export * from './order.dto';
+export * from './pet.dto';
+export * from './tag.dto';
+export * from './user.dto';
