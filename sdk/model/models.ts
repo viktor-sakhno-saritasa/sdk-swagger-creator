119af2881c120d63954f64b2e617ddc4bc5cf27b
Viktor Sakhno <viktor.sakhno@saritasa.com>
2023-07-25 18:23:45 +0700

    update

--- sdk/model/models.ts
+++
@@ -1,6 +0,0 @@
-export * from './api-response.dto';
-export * from './category.dto';
-export * from './order.dto';
-export * from './pet.dto';
-export * from './tag.dto';
-export * from './user.dto';
